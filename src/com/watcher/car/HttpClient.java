e3952c23359db34641c944232d034d9ab642c6a9
Jarmo Pertman <jarmo.p@gmail.com>
2014-04-09 01:15:38 +0300

    Use basic auth when posting data.

--- src/com/watcher/car/HttpClient.java
+++ src/com/watcher/car/HttpClient.java
@@ -1,15 +1,20 @@
 package com.watcher.car;
 
+import android.util.Base64;
+
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import static android.util.Base64.DEFAULT;
 
 public class HttpClient {
 
   public void post(String data) {
     try {
       HttpURLConnection conn = openConnection("http://leetor.no-ip.org:8010/");
+      conn.setRequestProperty("Authorization", "Basic " + Base64.encodeToString("car:watcher".getBytes(), DEFAULT));
       conn.setDoOutput(true);
       OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
       wr.write(data);
